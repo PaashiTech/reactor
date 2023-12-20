258e0613ebd62395e97e26c07c78293dd09fb15c
tiwasum <suman.tiwari@outlook.com>
2023-12-20 16:35:10 +0530

    working accessibility label

--- packages/views/src/App.tsx
+++ packages/views/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Fragment } from 'react';
+import { Button } from '@fluentui/react-components';
 
 function App(): React.JSX.Element {
 
     return (
 
         <Fragment>
+            <Button aria-label='Reactor'>Reactor</Button>
             {/* <Fragment accessibilityRole='heading'>Reactor</Fragment> */}
         </Fragment>
 
