e1c3280a372db9e3b8976b15ff3c07b27155169d
harshmohite-fradium <harsh.mohite@fradium.com>
2024-01-18 13:33:32 +0530

    starter code

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,12 +1,23 @@
-import { TamaguiProvider, config, Text } from 'unmaze-views';
+import { TamaguiProvider, config, Text, Theme } from "unmaze-views";
 
 export function App() {
   return (
-    <TamaguiProvider config={config}>
+    <TamaguiProvider config={config} defaultTheme="dark">
       {/* your app here */}
-	<Text>Some text is no longer here</Text>
+      <Text
+        fontSize="$10"
+        color="$color"
+        bg="$background"
+        p="$4"
+        letterSpacing={1}
+        borderWidth="$1"
+        m="$2"
+        borderColor="$borderColor"
+      >
+        Hello World
+      </Text>
     </TamaguiProvider>
-  )
-};
+  );
+}
 
-export default App
+export default App;
