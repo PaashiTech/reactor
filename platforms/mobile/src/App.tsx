ee024ed1ef400fac6277300145c3712128b92f0e
Harsh Mohite <148188131+harshmohite-fradium@users.noreply.github.com>
2024-01-24 23:32:54 +0530

    Unmaze Gradient Button component (#3)
    * create linear gradient button component
    
    * changed ReactNode to ReactElement in ButtonIcon
    
    React.cloneElement expects a type ReactElement as an argument.
    
    * create UnmzGradientButton

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,12 +1,36 @@
-import { config, TestComponent, LinearGradientComponent } from "@unmaze/views";
-import { TamaguiProvider, View, Button } from "tamagui";
+import { config } from "@unmaze/views";
+import { TamaguiProvider, Text, View } from "tamagui";
+import { LinearGradient } from "tamagui/linear-gradient";
+import { StatusBar } from "react-native";
+import { UnmzGradientButton } from "@unmaze/views";
+import { Plus } from "@tamagui/lucide-icons";
 
 export function App() {
   return (
     <TamaguiProvider config={config} defaultTheme="light">
       {/* your app here */}
-      <TestComponent />
-      <LinearGradientComponent />
+      <View
+        flex={1}
+        jc={"center"}
+        bg="$background"
+        gap={"$4"}
+        paddingHorizontal={24}
+      >
+        <StatusBar backgroundColor={"black"} />
+        {/* <ButtonComponent>Add number</ButtonComponent> */}
+        <UnmzGradientButton>
+          <UnmzGradientButton.Icon>
+            <Plus />
+          </UnmzGradientButton.Icon>
+          <UnmzGradientButton.Text>Add number</UnmzGradientButton.Text>
+        </UnmzGradientButton>
+        <UnmzGradientButton>
+          <UnmzGradientButton.Text>Continue</UnmzGradientButton.Text>
+        </UnmzGradientButton>
+        <UnmzGradientButton>
+          <UnmzGradientButton.Text>Confirm</UnmzGradientButton.Text>
+        </UnmzGradientButton>
+      </View>
     </TamaguiProvider>
   );
 }
