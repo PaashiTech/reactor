d4c765c98e961e126eff1858b465a05f5c9c5ab0
Harsh Mohite <harsh.mohite@fradium.com>
2024-01-31 12:22:50 +0530

    chore: cleanup UnmzGradientButton

--- packages/views/src/components/UnmzGradientButton/index.tsx
+++ packages/views/src/components/UnmzGradientButton/index.tsx
@@ -8,30 +8,6 @@
 import { Button, ButtonProps, Unspaced } from "tamagui";
 import { LinearGradient } from "tamagui/linear-gradient";
 
-// export const UnmzGradientButton: React.FC<ButtonProps> = (props) => {
-//   const { alignSelf, ...restProps } = props;
-//   return (
-//     <LinearGradient
-//       colors={["#fff000", "#ccfd62"]}
-//       locations={[0.05, 0.92]}
-//       start={[0.2, 1]}
-//       end={[1, 1]}
-//       borderRadius={9999}
-//       alignSelf={alignSelf}
-//     >
-//       <Button
-//         backgroundColor="transparent"
-//         color={"#262626"}
-//         hoverStyle={{ backgroundColor: "transparent" }}
-//         pressStyle={{ backgroundColor: "transparent" }}
-//         {...restProps}
-//       >
-//         {props.children}
-//       </Button>
-//     </LinearGradient>
-//   );
-// };
-
 export const UnmzGradientButton = Button.styleable((props, ref) => {
   return (
     <Button
