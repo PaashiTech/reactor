1e7c39de444baf117bf2a21c68b2be63eccc1fa0
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-01 15:33:21 +0530

    refactor: styles have types

--- packages/views/src/components/UnmzGradientButton/index.tsx
+++ packages/views/src/components/UnmzGradientButton/index.tsx
@@ -5,25 +5,24 @@
  * Button with a linear gradient of Unmaze brand colors
  */
 
-import { Button, ButtonProps, Unspaced } from "tamagui";
-import { LinearGradient } from "tamagui/linear-gradient";
-import { ButtonStyles, GradientStyles } from "./styles";
+import { Button, Unspaced } from "tamagui";
+import { UnmzButtonStyles, UnmzLinearGradientStyles } from "./styles";
+import { LinearGradient } from "@tamagui/linear-gradient";
 
-export const UnmzGradientButton = Button.styleable<ButtonProps>(
-  (props, ref) => {
-    return (
-      <Button size="$4" ref={ref} style={ButtonStyles} {...props}>
-        <Button.Text zIndex={1} fontWeight={"600"}>
-          {props.children}
-        </Button.Text>
-        <Unspaced>
-          <LinearGradient
-            style={GradientStyles}
-            $platform-native={{ zIndex: -1 }}
-            $platform-web={{ zIndex: 0 }}
-          />
-        </Unspaced>
-      </Button>
-    );
-  }
-);
+export const UnmzGradientButton = Button.styleable((props, ref) => {
+  return (
+    <Button size="$4" ref={ref} {...UnmzButtonStyles} {...props}>
+      <Button.Text zIndex={1} fontWeight={"600"}>
+        {props.children}
+      </Button.Text>
+      <Unspaced>
+        <LinearGradient
+          {...UnmzLinearGradientStyles}
+          fullscreen
+          $platform-native={{ zIndex: -1 }}
+          $platform-web={{ zIndex: 0 }}
+        />
+      </Unspaced>
+    </Button>
+  );
+});
