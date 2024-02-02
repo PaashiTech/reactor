d9c28c6f5eaa1b761a9632e8f5a6ea9f046cf792
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-02 19:50:00 +0530

    chore: top-level app cleanup

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,15 +1,12 @@
-import { TamaguiProvider, tamaguiConfig } from "@unmaze/views";
-import OtpScreen from "./screens/OtpScreen";
+import { TamaguiProvider, Text, tamaguiConfig } from "@unmaze/views";
 import { StatusBar } from "react-native";
-import { ProfileDetailsTest1 } from "./playground/List";
 
 export function App() {
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
       {/* your app here */}
       <StatusBar barStyle={"dark-content"} backgroundColor={"white"} />
-      {/* <OtpScreen /> */}
-      <ProfileDetailsTest1 />
+      <Text>Your app here</Text>
     </TamaguiProvider>
   );
 }
