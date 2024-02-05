6765a11567fd67854b63b61d2df4029940eeb636
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-05 23:00:57 +0530

    Added assets package

---
+++ packages/assets/fonts/index.ts
@@ -0,0 +1,26 @@
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+export const useFontsExpo = () => {
+  const [fontsLoaded] = useFonts({
+    // Tamagui built-ins
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+
+    // Custom
+    SatoshiVariable: require("./satoshi-variable/Satoshi-Variable.ttf"),
+    SatoshiRegular: require("./satoshi-variable/Satoshi-Regular.otf"),
+    SatoshiMedium: require("./satoshi-variable/Satoshi-Medium.otf"),
+    SatoshiBold: require("./satoshi-variable/Satoshi-Bold.otf"),
+    SatoshiLight: require("./satoshi-variable/Satoshi-Light.otf"),
+    SatoshiSemibold: require("./satoshi-variable/Satoshi-Semibold.otf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      // can hide splash screen here
+    }
+  }, [fontsLoaded]);
+
+  return fontsLoaded;
+};
---
+++ packages/assets/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The package that provides assets (icons, fonts, gifs etc.) to all the packages.
+ *
+ * @package assets
+ */
+
+// Fonts
+export * from "./fonts";
+
+// Icons
+export * as unmzIcons from "./icons";
