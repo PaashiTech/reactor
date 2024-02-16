fef99b027847beef6e85dbb63289e0517411617d
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-16 12:53:14 +0530

    chore: slight refactor of expo app and mobile app

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,12 +1,17 @@
 import { TamaguiProvider, tamaguiConfig } from "@unmaze/views";
+
 import { UnmzStackNavigator } from "./navigation/UnmzStackNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
 export function App() {
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
       {/* your app here */}
       {/* <Text>Your app here</Text> */}
-      <UnmzStackNavigator />
+
+      <NavigationContainer>
+        <UnmzStackNavigator />
+      </NavigationContainer>
     </TamaguiProvider>
   );
 }
