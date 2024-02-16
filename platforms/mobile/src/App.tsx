96b547185a0a0e0828a173cdc3b9f497bbdacecb
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-16 12:53:52 +0530

    chore: slight refactor (pt. 2)

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,9 +1,16 @@
 import { TamaguiProvider, tamaguiConfig } from "@unmaze/views";
+import { useUnmzFontsExpo } from "@unmaze/assets";
 
 import { UnmzStackNavigator } from "./navigation/UnmzStackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 
 export function App() {
+  const [fontsLoaded] = useUnmzFontsExpo();
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
       {/* your app here */}
