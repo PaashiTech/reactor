14a2eb6094db751b1ef0177c26a404531570b40e
Harsh Mohite <harsh.mohite@fradium.com>
2024-02-20 10:54:52 +0530

    fix: toast, tamagui versions

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -1,8 +1,17 @@
-import { TamaguiProvider, tamaguiConfig } from "@unmaze/views";
+import {
+  CustomToast,
+  TamaguiProvider,
+  View,
+  tamaguiConfig,
+} from "@unmaze/views";
 import { useUnmzFontsExpo } from "@unmaze/assets";
 import { UnmzStackNavigator } from "./navigation/UnmzStackNavigator";
-import { NavigationContainer } from "@react-navigation/native";
-
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
+import { ToastProvider, ToastViewport } from "@tamagui/toast";
 export function App() {
   const [fontsLoaded] = useUnmzFontsExpo();
 
@@ -15,9 +24,15 @@ export function App() {
       {/* your app here */}
       {/* <Text>Your app here</Text> */}
 
-      <NavigationContainer>
-        <UnmzStackNavigator />
-      </NavigationContainer>
+      {/* <NavigationContainer theme={DarkTheme}> */}
+      {/* <UnmzStackNavigator /> */}
+      <ToastProvider>
+        <View f={1} ai={"center"} jc={"center"} bg={"$background"}>
+          <CustomToast />
+        </View>
+        <ToastViewport left={0} right={0} bottom={50} />
+      </ToastProvider>
+      {/* </NavigationContainer> */}
     </TamaguiProvider>
   );
 }
