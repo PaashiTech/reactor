08bbd3eec890bdf22c1b8a8858f4f01a2cf8b35b
Harsh Mohite <148188131+harshmohite-fradium@users.noreply.github.com>
2024-02-22 14:58:25 +0530

    add keyboard avoiding view for form inputs

--- platforms/mobile/src/screens/family/AddFamilyMemberScreen.tsx
+++ platforms/mobile/src/screens/family/AddFamilyMemberScreen.tsx
@@ -5,26 +5,42 @@ import {
   FormTextInput,
   MobileNumberInput,
   CalendarPicker,
+  ScrollView,
 } from "@unmaze/views";
 import { Plus } from "@unmaze/assets";
 import { KeyBenefits } from "../../components/KeyBenefits";
 import { SelectRelationship } from "../../components/SelectRelationship";
+import { KeyboardAvoidingView } from "react-native";
 
 const _AddFamilyMemberScreen: React.FC<AddFamilyMemberScreenProps> = () => {
   return (
-    <View flex={1} p={20} jc="space-between">
-      <View gap={24}>
-        <KeyBenefits />
-        <FormTextInput label="First name" placeholder="Enter first name" />
-        <FormTextInput label="Last name" placeholder="Enter last name" />
-        <SelectRelationship />
-        <CalendarPicker />
-        <MobileNumberInput
-          mobileNumberValue=""
-          handleMobileNumberChange={() => {}}
-        />
-      </View>
-      <View gap={12}>
+    <View flex={1}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+        enabled
+        keyboardVerticalOffset={100}
+      >
+        <ScrollView flex={1}>
+          <View flex={1} p={20} justifyContent="space-between">
+            <View gap={24}>
+              <KeyBenefits />
+              <FormTextInput
+                label="First name"
+                placeholder="Enter first name"
+              />
+              <FormTextInput label="Last name" placeholder="Enter last name" />
+              <SelectRelationship />
+              <CalendarPicker />
+              <MobileNumberInput
+                mobileNumberValue=""
+                handleMobileNumberChange={() => {}}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <View gap={12} padding={20}>
         <UnmzGradientButton disabled>Invite using OTP</UnmzGradientButton>
         <UnmzGradientButton disabled icon={Plus}>
           Invite using Whatsapp
