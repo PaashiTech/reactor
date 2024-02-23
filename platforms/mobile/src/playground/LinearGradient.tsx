89f1766ace6c8d04a55f4a889f0b28adf7571018
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-23 22:54:01 +0530

    fix: brand gradient is now true to design

---
+++ platforms/mobile/src/playground/LinearGradient.tsx
@@ -0,0 +1,9 @@
+import { View, UnmzLinearGradient } from "@unmaze/views";
+
+export const LinearGradientTest = () => {
+  return (
+    <UnmzLinearGradient>
+      <View width={100} height={500}></View>
+    </UnmzLinearGradient>
+  );
+};
