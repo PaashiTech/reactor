2a6bf2ecf1a54b33e9084ee4027c17f4cad5901c
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-23 22:56:30 +0530

    wip: API mocking using msw

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -3,6 +3,10 @@ import { useUnmzFontsExpo } from "@unmaze/assets";
 import { NavigationContainer } from "@react-navigation/native";
 import { ToastProvider, ToastViewport } from "@tamagui/toast";
 import { FamilyStackNavigator } from "./navigation";
+
+import { APIGlobalConfigProvider } from "@unmaze/api";
+import { APITest } from "./playground/API";
+
 export function App() {
   const [fontsLoaded] = useUnmzFontsExpo();
 
@@ -11,13 +15,16 @@ export function App() {
   }
 
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
-      <ToastProvider duration={2000} swipeDirection="horizontal">
-        <NavigationContainer>
-          <FamilyStackNavigator />
-        </NavigationContainer>
-      </ToastProvider>
-    </TamaguiProvider>
+    <APIGlobalConfigProvider>
+      <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+        <ToastProvider duration={2000} swipeDirection="horizontal">
+          <NavigationContainer>
+            <FamilyStackNavigator />
+          </NavigationContainer>
+        </ToastProvider>
+        {/* <APITest /> */}
+      </TamaguiProvider>
+    </APIGlobalConfigProvider>
   );
 }
 
