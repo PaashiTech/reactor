00638f7f808fe487968012a9227d57c3c226caba
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-02-23 22:46:43 +0530

    Bare basic API example working

---
+++ packages/api/src/user/useUser.ts
@@ -0,0 +1,20 @@
+import useSWR from "swr";
+// const axios = require("axios").default;
+import { axiosInstance } from "../_core/axiosProvider";
+
+const fetcher = (url) => axiosInstance.get(url).then((res) => res.data);
+
+type GetUserInput = {};
+type GetUserParams = {
+  id: string;
+};
+
+export const useUser = (params: GetUserParams) => {
+  const { data, isLoading, error } = useSWR(`/user/${params.id}`, fetcher);
+
+  return {
+    userData: data,
+    userIsLoading: isLoading,
+    userError: error,
+  };
+};
