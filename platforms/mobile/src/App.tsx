14e228282143055ad34cbafedf9cc97744c651d1
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-03-02 19:07:56 +0530

    chore: removed playground stuff

--- platforms/mobile/src/App.tsx
+++ platforms/mobile/src/App.tsx
@@ -7,8 +7,6 @@ import { useUnmzFontsExpo } from "@unmaze/assets";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StackNavigator } from "./navigation/navigators/StackNavigator";
-import { PersistantGlobalState } from "./playground/PersistantGlobalState";
-import { APITest } from "./playground/API";
 
 export function App() {
   const [fontsLoaded] = useUnmzFontsExpo();
@@ -29,11 +27,9 @@ export function App() {
     <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
       <UnmzToastProvider>
         <SafeAreaProvider>
-          {/* <NavigationContainer theme={BaseTheme}>
+          <NavigationContainer theme={BaseTheme}>
             <StackNavigator />
-          </NavigationContainer> */}
-          {/* <PersistantGlobalState /> */}
-          <APITest />
+          </NavigationContainer>
         </SafeAreaProvider>
       </UnmzToastProvider>
     </TamaguiProvider>
