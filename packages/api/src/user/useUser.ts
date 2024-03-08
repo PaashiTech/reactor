1ad9b339db044686c732b14a0046e01922c9b6d0
Amogh Kulkarni <amogh.kulkarni@fradium.com>
2024-03-08 12:42:18 +0530

    feat: hook for updating the User

--- packages/api/src/user/useUser.ts
+++ packages/api/src/user/useUser.ts
@@ -4,6 +4,11 @@ import { axiosInstance } from "../core/axiosProvider";
 import { useUserStore, UserState } from "@unmaze/views";
 
 const _get = (url) => axiosInstance.get(url).then((res) => res.data);
+const _patch = (url) => axiosInstance.patch(url).then((res) => res.data);
+
+//////
+// Get User
+//////
 
 type GetUserQueryBody = {}; // placeholder
 type GetUserQueryParams = {}; // placeholder
@@ -11,9 +16,15 @@ type GetUserParams = {
   id: string;
 };
 
+/**
+ * Hook to get the User details, given the User ID
+ * @param params Contains User ID
+ * @returns {userData, userIsLoading, userError} data, loading and error states
+ * of the user data retrieval request sent to the server
+ */
 export const useUser = (params: GetUserParams) => {
   const setState = useUserStore((state) => state.setState);
-  const { data, isLoading, error } = useSWR<{ data: UserState }>(
+  const { data, isLoading, error, mutate } = useSWR<{ data: UserState }>(
     `/user/${params.id}`,
     _get,
     {
@@ -27,5 +38,46 @@ export const useUser = (params: GetUserParams) => {
     userData: data,
     userIsLoading: isLoading,
     userError: error,
+    userMutate: mutate,
+  };
+};
+
+//////
+// Update User
+//////
+
+type UpdateUserQueryBody = Partial<UserState> & {
+  otp: string;
+  session_id: string;
+};
+type UpdateUserQueryParams = {};
+type UpdateUserParams = {
+  id: string;
+  otp: string;
+  session_id: string;
+  user_details: Partial<UserState>;
+};
+
+/**
+ * Hook to update the protected fields of remote resource User
+ * @param params Contain OTP, session ID associated with that OTP and properties
+ * to be changed of the remote resource User
+ * @returns {userUpdateData, userUpdateIsLoading, userUpdateError, userUpdateMutate}
+ * response data, loading state, error state and the function to be called after the
+ * request returns successfully
+ */
+export const useUpdateUser = (params: UpdateUserParams) => {
+  // const setState = useUserStore((state) => state.setState);
+  const { userMutate } = useUser({ id: params.id });
+  const { data, isLoading, error } = useSWR(`/user/${params.id}`, _patch, {
+    onSuccess: () => {
+      userMutate();
+    },
+  });
+
+  return {
+    userUpdateData: data,
+    userUpdateIsLoading: isLoading,
+    userUpdateError: error,
   };
 };
