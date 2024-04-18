637c6b580b2f1c8dcf872849d6fe148e86cd7fe5
Harsh Mohite <148188131+harshmohite-unmaze@users.noreply.github.com>
2024-04-18 14:38:59 +0530

    Animated bottom tabs (#31)
    * add animation to bottom tabs
    
    * use useRef instead of useState for new animated values

--- platforms/mobile/src/navigation/navigators/UnmazeBottomTabNav.tsx
+++ platforms/mobile/src/navigation/navigators/UnmazeBottomTabNav.tsx
@@ -1,17 +1,59 @@
 import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { BodyText, SVGWrapper, ShadowWrapper, View } from "@unmaze/views";
-import { Pressable } from "react-native";
+import { useEffect, useRef } from "react";
+import { Animated, Dimensions, Pressable } from "react-native";
 
 interface UnmazeBottomTabNavProps extends BottomTabBarProps {}
+
+const width = Dimensions.get("window").width;
+const TAB_WIDTH = width / 4;
 
 export const UnmazeBottomTabNav: React.FC<UnmazeBottomTabNavProps> = ({
   state,
   descriptors,
   navigation,
 }) => {
+  const translateX = useRef(new Animated.Value(0)).current;
+
+  const translateTab = (index: number) => {
+    Animated.timing(translateX, {
+      toValue: index * TAB_WIDTH,
+      useNativeDriver: true,
+      duration: 400,
+    }).start();
+  };
+
+  useEffect(() => {
+    translateTab(state.index);
+  }, [state.index]);
+
   return (
     <ShadowWrapper size="md-top">
-      <View flexDirection="row" justifyContent="space-between" bg="#fff">
+      <View
+        flexDirection="row"
+        justifyContent="space-between"
+        bg="#fff"
+        position="relative"
+      >
+        <View
+          alignItems="center"
+          position="absolute"
+          width={TAB_WIDTH}
+          height={"100%"}
+        >
+          <Animated.View
+            style={{
+              width: 60,
+              height: 60,
+              backgroundColor: "#035E5D",
+              borderRadius: 30,
+              bottom: 20,
+              borderWidth: 4,
+              borderColor: "#fff",
+              transform: [{ translateX }],
+            }}
+          ></Animated.View>
+        </View>
         {state.routes.map((route, index) => {
           const { options } = descriptors[route.key];
 
@@ -19,18 +61,12 @@ export const UnmazeBottomTabNav: React.FC<UnmazeBottomTabNavProps> = ({
 
           return (
             <View key={route.key} flex={1}>
-              <Pressable
-                onPress={() => navigation.navigate(route.name)}
-                android_ripple={{
-                  borderless: true,
-                }}
-              >
+              <Pressable onPress={() => navigation.navigate(route.name)}>
                 <View alignItems="center" gap={8} paddingVertical={12}>
                   {options.tabBarIcon && (
-                    <SVGWrapper
-                      iconSVG={options.tabBarIcon}
-                      svgColor={isFocused ? "#035E5D" : "#697077"}
-                      size="lg"
+                    <AnimatedIcon
+                      isFocused={isFocused}
+                      tabBarIcon={options.tabBarIcon}
                     />
                   )}
                   <BodyText size="sm" fontWeight={isFocused ? "600" : "400"}>
@@ -43,5 +79,35 @@ export const UnmazeBottomTabNav: React.FC<UnmazeBottomTabNavProps> = ({
         })}
       </View>
     </ShadowWrapper>
+  );
+};
+
+const AnimatedIcon = ({ isFocused, tabBarIcon }) => {
+  const translateY = useRef(new Animated.Value(0)).current;
+
+  const translateIcon = (value: number) => {
+    Animated.timing(translateY, {
+      toValue: value,
+      useNativeDriver: true,
+      duration: 400,
+    }).start();
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      translateIcon(-14);
+    } else {
+      translateIcon(0);
+    }
+  }, [isFocused]);
+
+  return (
+    <Animated.View style={{ transform: [{ translateY }] }}>
+      <SVGWrapper
+        iconSVG={tabBarIcon}
+        svgColor={isFocused ? "#fff" : "#697077"}
+        size="lg"
+      />
+    </Animated.View>
   );
 };
