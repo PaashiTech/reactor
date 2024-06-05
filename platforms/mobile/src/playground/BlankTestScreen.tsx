dc3bc3f3d1f9080b6128385f219b78931520adbf
Harsh Mohite <harsh.mohite@unmaze.app>
2024-06-05 12:43:37 +0530

    add library

--- platforms/mobile/src/playground/BlankTestScreen.tsx
+++ platforms/mobile/src/playground/BlankTestScreen.tsx
@@ -2,6 +2,14 @@ import { Text, View, ViewProps } from "@unmaze/views";
 
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+import React, { useRef, useEffect } from "react";
+import * as echarts from "echarts/core";
+import { LineChart } from "echarts/charts";
+import { GridComponent } from "echarts/components";
+import { SVGRenderer, SkiaChart } from "@wuba/react-native-echarts";
+
+echarts.use([SVGRenderer, LineChart, GridComponent]);
+
 export const BlankTestScreen = () => {
   const insets = useSafeAreaInsets();
 
@@ -12,9 +20,38 @@ export const BlankTestScreen = () => {
     paddingRight: insets.right,
   };
 
+  const skiaRef = useRef<any>(null);
+  useEffect(() => {
+    const option = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: "line",
+        },
+      ],
+    };
+    let chart: any;
+    if (skiaRef.current) {
+      chart = echarts.init(skiaRef.current, "light", {
+        renderer: "svg",
+        width: 400,
+        height: 400,
+      });
+      chart.setOption(option);
+    }
+    return () => chart?.dispose();
+  }, []);
+
   return (
     <View {...safeAreaInsets} flex={1} ai="center" jc="center">
-      <Text>Blank Test Screen</Text>
+      <SkiaChart ref={skiaRef} />
     </View>
   );
 };
