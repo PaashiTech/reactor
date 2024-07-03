1ba1dd0ebfcf0980c62701c3e8fa3aa74bc600ad
Harsh Mohite <harsh.mohite@unmaze.app>
2024-07-03 12:21:38 +0530

    Update DailyLineChart.tsx

--- platforms/mobile/src/components/app/dashboard/cashflowScreen/DailyLineChart.tsx
+++ platforms/mobile/src/components/app/dashboard/cashflowScreen/DailyLineChart.tsx
@@ -1,68 +1,87 @@
 import React from "react";
 import { View, HeadingText } from "@unmaze/views";
 import { LineChart } from "react-native-gifted-charts";
+import { useCashflowScreenContext } from "./context/CashflowScreenContextProvider";
+
+type DataType = {
+  date: string;
+  value: number;
+};
+
+const ptData: DataType[] = [
+  { date: "1 May", value: 16190 },
+  { date: "2 May", value: 17200 },
+  { date: "3 May", value: 20040 },
+  { date: "4 May", value: 21310 },
+  { date: "5 May", value: 21310 },
+  { date: "6 May", value: 32030 },
+  { date: "7 May", value: 35350 },
+  { date: "8 May", value: 38050 },
+  { date: "9 May", value: 39670 },
+  { date: "10 May", value: 43820 },
+  { date: "11 May", value: 44150 },
+  { date: "12 May", value: 44150 },
+  { date: "13 May", value: 47150 },
+  { date: "14 May", value: 52340 },
+  { date: "15 May", value: 70620 },
+  { date: "16 May", value: 70870 },
+  { date: "17 May", value: 73810 },
+  { date: "18 May", value: 73810 },
+  { date: "19 May", value: 73810 },
+  { date: "20 May", value: 82480 },
+  { date: "21 May", value: 83030 },
+  { date: "22 May", value: 84170 },
+  { date: "23 May", value: 86380 },
+  { date: "24 May", value: 86380 },
+  { date: "25 May", value: 89710 },
+  { date: "26 May", value: 89710 },
+  { date: "27 May", value: 92120 },
+  { date: "28 May", value: 94140 },
+  { date: "29 May", value: 95610 },
+  { date: "30 May", value: 101000 },
+  { date: "31 May", value: 105000 },
+];
+
+const tabColors = {
+  0: {
+    primary: "#FFF1F1",
+    secondary: "#FFD7D9",
+  },
+  1: {
+    primary: "#E5F6FF",
+    secondary: "#BAE6FF",
+  },
+  2: {
+    primary: "#DEFBE6",
+    secondary: "#A7F0BA",
+  },
+};
 
 type DailyLineChartProps = {};
 
 const DailyLineChart: React.FC<DailyLineChartProps> = ({}) => {
-  const ptData = [
-    { value: 160, date: "1 Apr 2022" },
-    { value: 180, date: "2 Apr 2022" },
-    { value: 190, date: "3 Apr 2022" },
-    { value: 180, date: "4 Apr 2022" },
-    { value: 140, date: "5 Apr 2022" },
-    { value: 145, date: "6 Apr 2022" },
-    { value: 160, date: "7 Apr 2022" },
-    { value: 200, date: "8 Apr 2022" },
-    { value: 220, date: "9 Apr 2022" },
-    { value: 240, date: "10 Apr 2022" },
-    { value: 280, date: "11 Apr 2022" },
-    { value: 260, date: "12 Apr 2022" },
-    { value: 340, date: "13 Apr 2022" },
-    { value: 385, date: "14 Apr 2022" },
-    { value: 280, date: "15 Apr 2022" },
-    { value: 390, date: "16 Apr 2022" },
-    { value: 370, date: "17 Apr 2022" },
-    { value: 285, date: "18 Apr 2022" },
-    { value: 295, date: "19 Apr 2022" },
-    { value: 300, date: "20 Apr 2022" },
-    { value: 280, date: "21 Apr 2022" },
-    { value: 295, date: "22 Apr 2022" },
-    { value: 260, date: "23 Apr 2022" },
-    { value: 255, date: "24 Apr 2022" },
-    { value: 190, date: "25 Apr 2022" },
-    { value: 220, date: "26 Apr 2022" },
-    { value: 205, date: "27 Apr 2022" },
-    { value: 230, date: "28 Apr 2022" },
-    { value: 210, date: "29 Apr 2022" },
-    { value: 200, date: "30 Apr 2022" },
-    { value: 240, date: "1 May 2022" },
-    { value: 250, date: "2 May 2022" },
-    { value: 280, date: "3 May 2022" },
-    { value: 250, date: "4 May 2022" },
-    { value: 210, date: "5 May 2022" },
-  ];
-
+  const {
+    state: { activeTabIndex },
+  } = useCashflowScreenContext();
   return (
-    <View bg="#FFF">
+    <View bg="#FFF" pr="$3">
       <LineChart
+        adjustToWidth
         curved
         areaChart
         data={ptData}
         rotateLabel
         yAxisExtraHeight={35}
         hideDataPoints
-        spacing={10}
-        color="#6FDC8C"
         thickness={0}
-        startFillColor="#6FDC8C"
-        endFillColor="#DEFBE600"
+        startFillColor={tabColors[activeTabIndex].secondary}
+        endFillColor="#fff"
         startOpacity={0.9}
         endOpacity={0.2}
         initialSpacing={0}
         hideYAxisText
         hideAxesAndRules
-        maxValue={500}
+        maxValue={Math.max(...ptData.map((item) => item.value)) + 2000}
         pointerConfig={{
           pointerStripUptoDataPoint: true,
           pointerStripColor: "#666",
@@ -83,7 +102,7 @@ const DailyLineChart: React.FC<DailyLineChartProps> = ({}) => {
                   width={14}
                   height={14}
                   opacity={0.6}
-                  bg="#42BE65"
+                  bg={tabColors[activeTabIndex].secondary}
                   borderRadius={9999}
                 />
                 <View
@@ -92,7 +111,7 @@ const DailyLineChart: React.FC<DailyLineChartProps> = ({}) => {
                   left={3.5}
                   width={7}
                   height={7}
-                  bg="#24A148"
+                  bg={tabColors[activeTabIndex].primary}
                   borderRadius={9999}
                 />
               </View>
